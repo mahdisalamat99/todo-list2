7f6ff2f0f5300bc96555d8cb0ed39c46a4d9cc75
mahdi salamat <fannimh36@gmail.com>
2021-09-11 11:58:51 -0700

    ading edit

--- script.js
+++ script.js
@@ -1,73 +1,84 @@
 // getting all required elements
-const inputBox = document.querySelector('.inputField input');
-const addBtn = document.querySelector('.inputField button');
-const todoList = document.querySelector('.todoList');
-const deleteAllBtn = document.querySelector('.footer Button')
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer Button");
 
-inputBox.onkeyup = () =>{
-    let userData = inputBox.value; // getting user entered value
-    if(userData.trim() != 0){//if user values aren't only spaces
-        addBtn.classList.add('active');//active the add button
-    }else{
-        addBtn.classList.remove('active');//unactive the add button
-    }
-}
-showTasks();//calling showTask function
-
+inputBox.onkeyup = () => {
+  let userData = inputBox.value; // getting user entered value
+  if (userData.trim() != 0) {
+    //if user values aren't only spaces
+    addBtn.classList.add("active"); //active the add button
+  } else {
+    addBtn.classList.remove("active"); //unactive the add button
+  }
+};
+showTasks(); //calling showTask function
 
 // if user click on the add button
-addBtn.onclick = () =>{
-    let userData = inputBox.value;//getting user entered value
-    let getLocalStorage = localStorage.getItem("New Todo");//getting localStorage
-    if(getLocalStorage == null){//if local storage is null
-        listArr = [];//creating blank array
-    }else{
-        listArr = JSON.parse(getLocalStorage);//transforming json string into a js object
-    }
-    listArr.push(userData);//pushing or adding user data
-    localStorage.setItem("New Todo", JSON.stringify(listArr));//transforming js object into a js string
-    showTasks();//calling showTask function
-    addBtn.classList.remove('active');//unactive the add button
- 
-}
+addBtn.onclick = () => {
+  let userData = inputBox.value; //getting user entered value
+  let getLocalStorage = localStorage.getItem("New Todo"); //getting localStorage
+  if (getLocalStorage == null) {
+    //if local storage is null
+    listArr = []; //creating blank array
+  } else {
+    listArr = JSON.parse(getLocalStorage); //transforming json string into a js object
+  }
+  listArr.push(userData); //pushing or adding user data
+  localStorage.setItem("New Todo", JSON.stringify(listArr)); //transforming js object into a js string
+  showTasks(); //calling showTask function
+  addBtn.classList.remove("active"); //unactive the add button
+};
 
 // function to add li tags inside ul tag
-function showTasks(){
-    let getLocalStorage = localStorage.getItem("New Todo");//getting localStorage
-    if(getLocalStorage == null){//if local storage is null
-        listArr = [];//creating blank array
-    }else{
-        listArr = JSON.parse(getLocalStorage);//transforming json string into a js object
-    }
-    const pendingNumb = document.querySelector('.pendingNumb');
-    pendingNumb.textContent=listArr.length;//passing the length value in pendingNumb
-    if(listArr.length >0){
-        deleteAllBtn.classList.add('active')
-    }else{
-        deleteAllBtn.classList.remove('active')
-    }
-    let newLiTag = '';
-    listArr.forEach((element,index)=>{
-        newLiTag += `<li>${element} <span onclick="deleteTask(${index})" ><i class="fas fa-trash" ></i></span></li>`;
-    })
-    todoList.innerHTML = newLiTag;//adding new li tag inside ul tag
-    inputBox.value="";//once task added leave the input field blank
+function showTasks() {
+  let getLocalStorage = localStorage.getItem("New Todo"); //getting localStorage
+  if (getLocalStorage == null) {
+    //if local storage is null
+    listArr = []; //creating blank array
+  } else {
+    listArr = JSON.parse(getLocalStorage); //transforming json string into a js object
+  }
+  const pendingNumb = document.querySelector(".pendingNumb");
+  pendingNumb.textContent = listArr.length; //passing the length value in pendingNumb
+  if (listArr.length > 0) {
+    deleteAllBtn.classList.add("active");
+  } else {
+    deleteAllBtn.classList.remove("active");
+  }
+  let newLiTag = "";
+  listArr.forEach((element, index,edit) => {
+    newLiTag += `<li>${element} <span onclick="deleteTask(${index})" ><i class="fas fa-trash" ></i></span>
+    <span class="edit" onclick="editTask(${edit})" ><i class="fas fa-pen"></i></span>
+
+        </li>`;
+  });
+  todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
+  inputBox.value = ""; //once task added leave the input field blank
 }
 
-//delete task function 
-function deleteTask(index){
-    let getLocalStorage = localStorage.getItem("New Todo");//getting localStorage
-    listArr = JSON.parse(getLocalStorage);
-    listArr.splice(index,1);//delete or remove the particular index li
-    //after remove li again update the local storage
-    localStorage.setItem("New Todo", JSON.stringify(listArr));//transforming json string into a js object
-    showTasks();
+//delete task function
+function deleteTask(index) {
+  let getLocalStorage = localStorage.getItem("New Todo"); //getting localStorage
+  listArr = JSON.parse(getLocalStorage);
+  listArr.splice(index, 1); //delete or remove the particular index li
+  //after remove li again update the local storage
+  localStorage.setItem("New Todo", JSON.stringify(listArr)); //transforming json string into a js object
+  showTasks();
+  console.log("yep1")
 }
 
 // delete all tasks function
-deleteAllBtn.onclick = () =>{
-    listArr = [];//empty an array
-    //after delete all tasks again update the local storage
-    localStorage.setItem("New Todo", JSON.stringify(listArr));//transforming json string into a js object
-    showTasks();
+deleteAllBtn.onclick = () => {
+  listArr = []; //empty an array
+  //after delete all tasks again update the local storage
+  localStorage.setItem("New Todo", JSON.stringify(listArr)); //transforming json string into a js object
+  showTasks();
+};
+
+
+// edit task function
+function editTask(edit){
+    console.log("yep2")
 }
